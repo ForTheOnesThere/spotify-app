ef7bb61a3550953429235f78dc36d8400330f7a0
ForTheOnesThere <jamescockayne1@gmail.com>
2021-01-09 23:13:58 +0000

    Added spotify login thorugh OAuth and extracted code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        This is working! Hello world!
-      </p>
-    </div>
-  );
+const clientId = 'ebcbc13ca3b34ed6a4cf0bf4d7579df9';
+const redirect = 'http%3A%2F%2Flocalhost:3000%2F';
+
+
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      code: ''
+    }
+  }
+
+  componentDidMount(){
+    this.parseUrl()
+  }
+
+  parseUrl = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    this.setState({code: urlParams.get('code')})
+    return
+  }
+
+  render(){
+    return (
+      this.state.code===null
+      ? <div className="App">
+          <button onClick={()=>window.location.replace(`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirect}&show_dialog=true`)}>Connect to Spotify</button>
+        </div>
+      : <div className="App">
+          <p>
+            Connected to Spotify.
+          </p>
+          <p>
+            The code is {this.state.code}
+          </p>
+        </div>
+    )
+  }
 }
 
 export default App;
