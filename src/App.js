c68667b72f5e55fc484da57aa4377a78d76a1ee3
ForTheOnesThere <jamescockayne1@gmail.com>
2021-01-09 23:23:21 +0000

    Converted to use react hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const clientId = 'ebcbc13ca3b34ed6a4cf0bf4d7579df9';
 const redirect = 'http%3A%2F%2Flocalhost:3000%2F';
 
+const App = () => {
 
+  const [code, setCode] = useState('')
 
-class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      code: ''
-    }
-  }
-
-  componentDidMount(){
-    this.parseUrl()
-  }
-
-  parseUrl = () => {
+  const parseUrl = () => {
     const urlParams = new URLSearchParams(window.location.search);
-    this.setState({code: urlParams.get('code')})
+    setCode(urlParams.get('code'))
     return
   }
 
-  render(){
-    return (
-      this.state.code===null
-      ? <div className="App">
-          <button onClick={()=>window.location.replace(`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirect}&show_dialog=true`)}>Connect to Spotify</button>
-        </div>
-      : <div className="App">
-          <p>
-            Connected to Spotify.
-          </p>
-          <p>
-            The code is {this.state.code}
-          </p>
-        </div>
-    )
-  }
+  useEffect(() => {
+    parseUrl()
+  },[])
+  
+  return (
+    code===null
+    ? <div className="App">
+        <button onClick={()=>window.location.replace(`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirect}&show_dialog=true`)}>Connect to Spotify</button>
+      </div>
+    : <div className="App">
+        <p>
+          Connected to Spotify.
+        </p>
+        <p>
+          The code is {code}
+        </p>
+      </div>
+  )
+  
 }
 
 export default App;
